7bc9a4083dd73cfea20efde8ed526470008c34ec
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-05-21 10:15:45 +0000

    bugs fixed

--- src/models.py
+++ src/models.py
@@ -10,55 +10,52 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    lastname = Column(String(50), nullable=False, unique=True)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
 
 class Characters(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    gender = Column(String(250))
-    birth_year = Column(String(250), nullable=False)
-    
-    user_id = Column(Integer, ForeignKey('user.id')),
-    user = relationship('User')
+    name = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    birth_year = Column(String(50), nullable=False)
 
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    population = Column(String(250))
+    name = Column(String(50), nullable=False)
+    population = Column(String(250), nullable=False)
     diameter = Column(String(250), nullable=False)
-
-    user_id = Column(Integer, ForeignKey('user.id')),
-    user = relationship('User')
 
 class Vehicles(Base):
     __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    model = Column(String(250))
+    name = Column(String(50), nullable=False)
+    model = Column(String(50), nullable=False)
     speed = Column(String(250), nullable=False)
-    
-    user_id = Column(Integer, ForeignKey('user.id')),
-    user = relationship('User')
+ 
 
-class Favorites(Base):
-    __tablename__ = 'favorites'
+class FavoritesCaracters(Base):
+    __tablename__ = 'favoritesCaracters'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
 
-    character_id = Column(Integer, ForeignKey('characters.id')),
-    character = relationship('Characters')
 
-    planet_id = Column(Integer, ForeignKey('planets.id')),
-    character = relationship('Planets')
+class FavoritesPlanets(Base):
+    __tablename__ = 'favoritesPlanets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
 
-    vehicle_id = Column(Integer, ForeignKey('vehicles.id')),
-    character = relationship('Vehicles')
 
-    user_id = Column(Integer, ForeignKey('user.id')),
-    character = relationship('User')
+class FavoritesVehicles(Base):
+    __tablename__ = 'favoritesVehicles'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+
 
     def to_dict(self):
         return {}
